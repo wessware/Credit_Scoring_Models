21c325da2ca1681ff44973d4331223485ae5701a
wessware <stevenshisia@yahoo.com>
2026-02-18 10:02:28 +0300

    advanced insight 3 - aprroval by education level and employment status

--- loans.sql
+++ loans.sql
@@ -146,3 +146,11 @@ SUM(CASE WHEN Loan_Status=1 THEN 1 ELSE 0 END)*100/COUNT(*) as Default_Rate
 from loan_data_clean
 
 GROUP BY Property_Area;
+
+
+SELECT Education, Self_Employed, 
+
+SUM(CASE WHEN Loan_Status=1 THEN 1 ELSE 0 END)*100/COUNT(*) as 
+LOAN_APPROVAL_RATE from loan_data_clean
+
+GROUP BY Education, Self_Employed;
--- loans_sql_pad.sql
+++ loans_sql_pad.sql
@@ -1,7 +1,7 @@
 
+SELECT Education, Self_Employed, 
 
-SELECT Property_Area, 
-SUM(CASE WHEN Loan_Status=1 THEN 1 ELSE 0 END)*100/COUNT(*) as Default_Rate 
-from loan_data_clean
+SUM(CASE WHEN Loan_Status=1 THEN 1 ELSE 0 END)*100/COUNT(*) as 
+LOAN_APPROVAL_RATE from loan_data_clean
 
-GROUP BY Property_Area;
+GROUP BY Education, Self_Employed;

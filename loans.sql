8cfdab009c62c59d4d74ed240c1d209dc6c8c826
wessware <stevenshisia@yahoo.com>
2026-02-18 08:41:01 +0300

    SQL updates

--- loans.sql
+++ loans.sql
@@ -5,3 +5,28 @@ SELECT * from loan_data_clean
 
 --view the total number of loans in the dataset
 SELECT COUNT(*) as total_loans FROM loan_data_clean;
+
+--view the unique loan statuses in the dataset
+SELECT DISTINCT Loan_Status FROM loan_data_clean;
+
+--view the count of each loan status in the dataset
+SELECT Loan_Status, COUNT(*) as count FROM loan_data_clean GROUP BY Loan_Status;
+
+--view the minimum, maximum, average loan amount 
+SELECT Loan_Status, 
+MIN(LoanAmount) as Minimum, 
+MAX(LoanAmount) as Maximum, 
+AVG(LoanAmount) as Average FROM loan_data_clean 
+
+GROUP BY Loan_Status;
+
+--view the average applicant income 
+SELECT AVG(ApplicantIncome) as Average_Income FROM loan_data_clean;
+
+SELECT Loan_Status,
+AVG(AppliantIncome) as Average_Income from loan_data_clean
+GROUP BY Loan_Status;
+
+SELECT Loan_Status,
+AVG(LoanAmount) as Average_Loan_Amount from loan_data_clean
+GROUP BY Loan_Status;
